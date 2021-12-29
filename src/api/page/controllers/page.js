b19f442d10c2bda3c5c93afacf0399bd0154ec0e
Sean Harte <scsharte@gmail.com>
2021-12-29 01:23:46 -0500

    find one with slug

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController('api::page.page', ({ strapi }) =>  ({
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+    const { query } = ctx;
+
+    const entity = await strapi.db.query('api::page.page').findOne({
+      ...query,
+      where: {
+        slug
+      }
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
