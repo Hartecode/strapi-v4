7434570095b5d66d88fa497503e1ef022c9d1258
Sean Harte <scsharte@gmail.com>
2022-01-07 13:47:22 -0500

    email set up

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,26 @@
-module.exports = {
+module.exports = ({ env }) =>  ({
   // ...
   'wysiwyg': {
     enabled: true,
     resolve: './src/plugins/wysiwyg'
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('DEFAULT_FROM'),
+        defaultReplyTo: env('DEFAULT_REPLY_TO'),
+      },
+    },
+  },
   // ...
-}
+})
