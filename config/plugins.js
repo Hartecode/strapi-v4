dfa8357e45402ad566d2d948f6072b9818e23085
hartes <hartes@orvis.com>
2022-03-01 10:27:39 -0500

    Revert "include cashe"
    This reverts commit 697b00f7e48574505ee51f05746ceb8be44117df.

--- config/plugins.js
+++ config/plugins.js
@@ -31,24 +31,7 @@ module.exports = ({ env }) =>  ({
         },
         allowedLevels: 2,
     }
-  },
-  "rest-cache": {
-    config: {
-      provider: {
-        name: "memory",
-        options: {
-          max: 32767,
-          maxAge: 3600,
-        },
-      },
-      strategy: {
-        contentTypes: [
-          // list of Content-Types UID to cache
-          "api::category.category",
-        ],
-      },
-    },
-  },
+},
   'wysiwyg': {
     enabled: true,
     resolve: './src/plugins/wysiwyg'
